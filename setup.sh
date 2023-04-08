3e8a56662e1c7549618e75625740b7465061f301
fcolic89 <filip.colic00@gmail.com>
2023-04-09 01:48:10 +0200

    Fixed linking setup file.

--- setup.sh
+++ setup.sh
@@ -7,12 +7,11 @@ sudo apt install git htop zsh curl tig neovim
 #Create links for all dotfiles
 for DOTFILE in `find ~/.dotfiles -maxdepth 1 -type f`
 do
-    #sudo ln -sfv $DOTFILE ~ 
-    if [ $DOTFILE != ".setup.sh" ]; then
+    if [[ $DOTFILE != */setup.sh ]]; then
     	#echo $DOTFILE
 	sudo ln -sfv $DOTFILE ~
     fi
 done
 
 #Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

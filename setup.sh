e2688b623620e5718a659244d559af86f9462489
fcolic89 <filip.colic00@gmail.com>
2023-04-09 13:46:54 +0200

    Fixed file linking, added color to text.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #Variables
+name="SETUP.SH"
 programs="git htop zsh curl tig neovim"
 dir=~/.dotfiles
 olddir=~/.dotfiles_old
 files="bashrc vimrc vim zshrc oh-my-zsh"
+color='\033[0;33m' #yellow
+nocolor='\033[0m' # No Color
 
 #Backing up old dotfiles
+echo -e "${color}[$name] Backing up old dot files!${nocolor}"
 mkdir -p $olddir
 cd $dir
 for file in $files; do
@@ -15,19 +19,21 @@ for file in $files; do
 done
 
 #Install programs
+echo -e "${color}[$name] Installing programs!${nocolor}"
 sudo apt update
-for prog in $programs; do
-	sudo apt install $prog
-done
+sudo apt install $programs
 
 #Install oh-my-zsh
+echo -e "${color}[$name] Installing oh-my-zsh!${nocolor}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #Create links for all dotfiles
+echo -e "${color}[$name] Creating links to dotfiles!${nocolor}"
 for DOTFILE in `find ~/.dotfiles -maxdepth 1 -type f`
 do
-    if [[ $DOTFILE != */setup.sh ]]; then
+    filename=${DOTFILE##*/}
+    if [[ $filename != setup.sh ]]; then
     	#echo $DOTFILE
-	sudo ln -sfv $dir/$DOTFILE ~/,$DOTFILE
+	sudo ln -sfv $dir/$filename ~/".$filename"
     fi
 done

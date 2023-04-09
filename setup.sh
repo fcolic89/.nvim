fdf9942444f52f56ea41dbdbdaf6c0a8053ed16d
fcolic89 <filip.colic00@gmail.com>
2023-04-09 13:09:04 +0200

    Changed setup.sh, backing up old dotfiles now.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+#Variables
+programs="git htop zsh curl tig neovim"
+dir=~/.dotfiles
+olddir=~/.dotfiles_old
+files="bashrc vimrc vim zshrc oh-my-zsh"
+
+#Backing up old dotfiles
+mkdir -p $olddir
+cd $dir
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+done
+
 #Install programs
 sudo apt update
-sudo apt install git htop zsh curl tig neovim
+for prog in $programs; do
+	sudo apt install $prog
+done
 
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -12,6 +28,6 @@ for DOTFILE in `find ~/.dotfiles -maxdepth 1 -type f`
 do
     if [[ $DOTFILE != */setup.sh ]]; then
     	#echo $DOTFILE
-	sudo ln -sfv $DOTFILE ~
+	sudo ln -sfv $dir/$DOTFILE ~/,$DOTFILE
     fi
 done

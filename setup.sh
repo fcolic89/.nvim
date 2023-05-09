f32c85730024387f2ffffac66d0f3c2ec9790c81
fcolic89 <filip.colic00@gmail.com>
2023-05-09 10:59:21 +0200

    Fixed the wrong plugin being installed.

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ install_plugins (){
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
     info "Installing syntax highlighting plugin"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
 install_custom_theme(){

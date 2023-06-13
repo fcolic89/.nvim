dd19194b935a305c7368d88041cdd798b3b45757
fcolic89 <filip.colic00@gmail.com>
2023-06-13 09:22:44 +0200

    Added ranger to the install list.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #Variables
 name="SETUP.SH"
-programs="git htop zsh curl tig neovim tmux fzf"
+programs="git htop zsh curl tig neovim tmux fzf ranger"
 dir="$HOME/.dotfiles"
 old_dir="$HOME/.old-dotfiles"
 custom_theme="attempt"

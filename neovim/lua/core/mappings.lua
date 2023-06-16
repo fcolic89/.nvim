424351bb7a286414d880cf37ba3704efcdd15407
fcolic89 <filip.colic00@gmail.com>
2023-06-16 16:21:41 +0200

    Added new keymapings.

--- neovim/lua/core/mappings.lua
+++ neovim/lua/core/mappings.lua
@@ -4,3 +4,5 @@ local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 keymap("n", "<leader>q", ":q<CR>", opts)
 keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>e", ":tabnew | Ex<CR>", opts)
+keymap("n", "<leader>c", ":bd<CR>", opts)

77246ce14ea9da71dacaebd8d208c76102de6d4f
fcolic89 <filip.colic00@gmail.com>
2023-07-04 12:29:13 +0200

    Neovim plugins.

--- neovim/lua.slink/plugin/plugins.lua
+++ neovim/lua.slink/plugin/plugins.lua
@@ -2,7 +2,17 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- plugins go here
-    use 'preservim/nerdtree'
+    -- use 'preservim/nerdtree'
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.2',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
     use 'folke/tokyonight.nvim'
     use 'ms-jpq/coq_nvim'
     use 'nvim-treesitter/nvim-treesitter'

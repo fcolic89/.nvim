c20da9d30d3d64fe72d073bfdeed6f67b663e9c3
fcolic <fcolic8920rn@raf.rs>
2023-07-10 22:56:43 +0200

    Another neovim change.

--- neovim/lua.slink/core/set.lua
+++ neovim/lua.slink/core/set.lua
@@ -7,6 +7,7 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wildmode = {"longest","list"}
 vim.opt.mouse = "a"

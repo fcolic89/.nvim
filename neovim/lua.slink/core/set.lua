37888cc3560a8254753f5d699632cdf5f8cf9384
fcolic89 <filip.colic00@gmail.com>
2024-02-05 13:30:03 +0100

    (neovim): changes in neovim settings

--- neovim/lua.slink/core/set.lua
+++ neovim/lua.slink/core/set.lua
@@ -2,14 +2,15 @@ vim.opt.showmatch = true
 vim.opt.ignorecase = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wildmode = {"longest","list"}
+vim.opt.wildmode = {"full"}
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
@@ -17,5 +18,8 @@ vim.opt.ttyfast = true
 vim.opt.confirm = true
 vim.opt.showcmd = true
 vim.opt.wrap = false
+vim.opt.autoread = true
 
-vim.cmd[[colorscheme tokyonight]]
+if not pcall(vim.cmd.colorscheme, 'tokyonight') then
+    vim.cmd.colorscheme('habamax')
+end

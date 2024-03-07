17581c906dacb89a27593edac3bd51168a01ad13
fcolic89 <filip.colic00@gmail.com>
2024-03-07 15:14:59 +0100

    (neovim): autocmd for formatting text

--- neovim/init.slink.lua
+++ neovim/init.slink.lua
@@ -9,29 +9,30 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-
 -- lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-    spec = {
-        { import = 'plugin' },
-    },
-    defaults = {
-        lazy = false,
-    },
-    change_detection = {
-        notify = false,
-    },
+  spec = {
+    { import = 'plugin' },
+  },
+  defaults = {
+    lazy = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 }, {})
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]

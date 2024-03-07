8a17c211f79cf6efe7e73daa7111b732e5a85ea0
fcolic89 <filip.colic00@gmail.com>
2024-03-07 15:22:37 +0100

    (neovim): fixed telescope not ignoring node_modules

--- neovim/lua.slink/plugin/telescope.lua
+++ neovim/lua.slink/plugin/telescope.lua
@@ -1,10 +1,14 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    defaults = {
-      file_ignore_patterns = {
-        "node_modules"
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules"
+        }
       }
     }
-  }
+  end
+}

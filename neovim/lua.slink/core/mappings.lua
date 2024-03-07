24f3ef96ebcba3cb073c1db1f6a11fed6be4f0d1
fcolic89 <filip.colic00@gmail.com>
2024-03-07 13:13:39 +0100

    (neovim): mappings updated

--- neovim/lua.slink/core/mappings.lua
+++ neovim/lua.slink/core/mappings.lua
@@ -1,43 +1,41 @@
-local opts1 = {noremap=true, silent=true}
-local opts2 = {silent=true}
-local keymap = vim.api.nvim_set_keymap
+local opts = {noremap=true, silent=true}
+local keymap = vim.keymap.set
 
 vim.g.mapleader = " "
 
 -- NORMAL MODE KEYMAPS
-keymap("n", "<leader>q", ":q<CR>", opts1)
-keymap("n", "<leader>w", ":w<CR>", opts1)
-keymap("n", "<leader>j", ":belowright split | startinsert | te <CR>", opts1)
-keymap("n", "<leader>l", ":vsplit | startinsert | te <CR>", opts1)
-keymap("n", "<leader>T", ":te<CR>", opts1)
+keymap("n", "<leader>q", ":q<CR>", opts)
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>Q", ":q!<CR>", opts)
+
+keymap("n", "<leader>j", ":belowright split | startinsert | te <CR>", opts)
 -- keymap("n", "<leader>e", ":tabnew | Ex<CR>", opts)
 -- keymap("n", "<leader>E", ":Ex<CR>", opts)
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts1)
-keymap("n", "<leader>o", ":NvimTreeFocus<CR>", opts1)
-keymap("n", "<leader>c", ":bd<CR>", opts1)
-keymap("n", "[", ":tabprevious<CR>", opts1)
-keymap("n", "]", ":tabnext<CR>", opts1)
-keymap("n", "<leader>v", ":vsplit<CR>", opts1)
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts1)
-keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts1)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>o", ":NvimTreeFocus<CR>", opts)
+keymap("n", "<leader>c", ":bd<CR>", opts)
+keymap("n", "<leader>k", ":ls<CR>", opts)
+keymap("n", "[[", ":bprev<CR>", opts)
+keymap("n", "]]", ":bnext<CR>", opts)
+keymap("n", "<C-\\>", ":vsplit<CR>", opts)
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>[", ":tabprev<CR>", opts)
+keymap("n", "<leader>]", ":tabnext<CR>", opts)
 
-keymap("n", "<A-h>", "<C-w>h", opts1)
-keymap("n", "<A-j>", "<C-w>j", opts1)
-keymap("n", "<A-k>", "<C-w>k", opts1)
-keymap("n", "<A-l>", "<C-w>l", opts1)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>/", "gcc<CR>", opts2)
-
-
--- VISUAL MODE KEYMAPS
-keymap("v", "<leader>/", "gc<CR>", opts2)
-
+-- INSERT MODE KEYMAPS
+keymap("i", "<C-c>", "<Esc>", opts)
 
 -- TERMINAL MODE KEYMAPS
-keymap("t", "<Esc>", "<C-\\><C-n><CR>", opts2)
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h<CR>", opts2)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j<CR>", opts2)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k<CR>", opts2)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l<CR>", opts2)
+keymap("t", "<Esc>", "<C-\\><C-n><CR>", opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h<CR>", opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j<CR>", opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k<CR>", opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l<CR>", opts)
 
 

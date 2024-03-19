72f8ab7e24fef62664080c5fb6211a8b28d4ab3e
fcolic89 <filip.colic00@gmail.com>
2024-03-19 12:45:54 +0100

    (neovim): code actions with telescope

--- neovim/lua.slink/plugin/telescope.lua
+++ neovim/lua.slink/plugin/telescope.lua
@@ -1,15 +1,23 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
   config = function()
     require('telescope').setup {
+      extensions = {
+        ["ui-select"] = {
+          require('telescope.themes').get_dropdown {}
+        }
+      },
       defaults = {
         file_ignore_patterns = {
           "node_modules"
         }
       }
     }
+
+    require('telescope').load_extension('ui-select')
+
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
   end

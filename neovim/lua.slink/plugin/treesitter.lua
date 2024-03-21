4a94631bacbe4c5b33049d7cf376d4ebb085825b
fcolic89 <filip.colic00@gmail.com>
2024-03-21 10:50:25 +0100

    (neovim): treesitter-context plugin added

--- neovim/lua.slink/plugin/treesitter.lua
+++ neovim/lua.slink/plugin/treesitter.lua
@@ -1,25 +1,35 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "tsx",
-        "json",
-        "go",
-        "javascript",
-        "html",
-        "python",
-        "bash"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+      configs.setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "tsx",
+          "json",
+          "go",
+          "javascript",
+          "html",
+          "python",
+          "bash"
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require('treesitter-context').setup {
+        enable = true
+      }
+    end
+  }
 }

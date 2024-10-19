36fb198f8fbd4b971cc24d10bed860941e2aee1e
fcolic89 <filip.colic00@gmail.com>
2024-10-19 18:42:01 +0200

    (config.sh): updated location of config files

--- config.sh
+++ config.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-dotfiles_dir=$HOME/.dotfiles/nvim
-dir=$HOME/.config/nvim
+files_dir=$HOME/.nvim
+install_dir=$HOME/.config/nvim
 backup_dir=$HOME/.config/.old-nvim-config
 
-help_string(){
-  cat << EOF
+help_string() {
+  cat <<EOF
   --install, -i      -> create symbolic links to all neovim files
   --uninstall, -u    -> remove all symbolic links to neovim files and return to previous configuration if one exists
   --help, -h         -> help
@@ -15,45 +15,43 @@ EOF
   return 0
 }
 
-
-install(){
+install() {
   #copy old config files
-  if [ -d "$dir" ]; then
-    mv $dir $backup_dir > /dev/null
+  if [ -d "$install_dir" ]; then
+    mv "$install_dir" "$backup_dir" >/dev/null
   fi
 
-  mkdir -p $dir
+  mkdir -p "$install_dir"
 
-  file_names=$(ls "$dotfiles_dir") 
+  file_names=$(ls "$files_dir")
 
   #link files
   substring_to_remove=".slink"
-  for name in $file_names
-  do
+  for name in $file_names; do
     if [[ "$name" != *"slink"* ]]; then
-        continue
+      continue
     fi
-    dst="$dir/${name/$substring_to_remove}"
-    ln -sfv "$dotfiles_dir/$name" "$dst"
+    dst="$install_dir/${name/$substring_to_remove/}"
+    ln -sfv "$files_dir/$name" "$dst"
   done
-  
+
   return 0
 }
 
-uninstall(){
+uninstall() {
   # remove lazy and all plugins
   rm -rf ~/.local/share/nvim/
   # remove all symbolic links to config files
   rm -rf ~/.config/nvim
   #return the old config
   if [ -d "$backup_dir" ]; then
-    mv $backup_dir $dir                         
+    mv "$backup_dir" "$install_dir"
   fi
 
   return 0
 }
 
-main(){
+main() {
   if [ "$1" == "--install" ] || [ "$1" == "-i" ]; then
     install
   elif [ "$1" == "--uninstall" ] || [ "$1" == "-u" ]; then
@@ -66,4 +64,4 @@ main(){
   fi
 }
 
-main $1
+main "$1"

6ccac2ea0803487fb2b9dd0e00843080e6552ee7
fcolic89 <filip.colic00@gmail.com>
2025-02-09 18:58:25 +0100

    (v0.2.0): Lackluster is the new default theme

--- lua.slink/plugin/colorscheme.lua
+++ lua.slink/plugin/colorscheme.lua
@@ -3,9 +3,16 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme('kanagawa-dragon')
-    end
+    -- config = function()
+    -- vim.cmd.colorscheme('kanagawa-dragon')
+    -- end
   },
+  {
+    "slugbyte/lackluster.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme("lackluster-hack")
+    end,
+  }
 }

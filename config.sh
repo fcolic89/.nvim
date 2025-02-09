1ab2ad040ac0ff2840d14f56e2116e7a25278d21
fcolic89 <filip.colic00@gmail.com>
2025-02-09 18:16:32 +0100

    (v0.2.0): Updated config script
    -Chnaged argument parsing in the config script

--- config.sh
+++ config.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-files_dir=$HOME/.nvim
-install_dir=$HOME/.config/nvim
-backup_dir=$HOME/.config/.old-nvim-config
+install_dir="$HOME/.config/nvim"
+backup_dir="$HOME/.config/.old-nvim-config"
 
-help_string() {
+usage() {
   cat <<EOF
+config.sh [OPTIONS]
+
+Options:
   --install, -i      -> create symbolic links to all neovim files
   --uninstall, -u    -> remove all symbolic links to neovim files and return to previous configuration if one exists
   --help, -h         -> help
@@ -16,52 +18,65 @@ EOF
 }
 
 install() {
-  #copy old config files
-  if [ -d "$install_dir" ]; then
+  echo "==> Installing neovim config"
+
+  if [[ -d "$install_dir" ]]; then
     mv "$install_dir" "$backup_dir" >/dev/null
   fi
 
   mkdir -p "$install_dir"
 
-  file_names=$(ls "$files_dir")
+  local config_location="$(pwd)/$(dirname -- "$0")"
+  file_names=$(ls "$config_location")
 
-  #link files
-  substring_to_remove=".slink"
+  local substring_to_remove=".slink"
   for name in $file_names; do
     if [[ "$name" != *"slink"* ]]; then
       continue
     fi
-    dst="$install_dir/${name/$substring_to_remove/}"
-    ln -sfv "$files_dir/$name" "$dst"
+    local dst="$install_dir/${name/$substring_to_remove/}"
+    ln -sfv "$config_location/$name" "$dst"
   done
 
   return 0
 }
 
 uninstall() {
-  # remove lazy and all plugins
+  echo "==> Uninstalling neovim config"
+
   rm -rf ~/.local/share/nvim/
-  # remove all symbolic links to config files
   rm -rf ~/.config/nvim
-  #return the old config
-  if [ -d "$backup_dir" ]; then
+
+  echo "==> Restoring backup version of the config"
+  if [[ -d "$backup_dir" ]]; then
     mv "$backup_dir" "$install_dir"
   fi
 
   return 0
 }
 
-main() {
-  if [ "$1" == "--install" ] || [ "$1" == "-i" ]; then
-    install
-  elif [ "$1" == "--uninstall" ] || [ "$1" == "-u" ]; then
-    uninstall
-  elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-    help_string
-  else
-    echo "Missing params. Type --help to see all options"
-    return 1
-  fi
-}
+[[ $# -eq 0 ]] && usage && exit 1
 
-main "$1"
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -i | --install)
+    install
+    shift
+    ;;
+  -u | --uninstall)
+    uninstall
+    shift
+    ;;
+  -h | --help)
+    usage
+    shift
+    ;;
+  --)
+    break
+    ;;
+  *)
+    echo "==> Missing a valid parameter"
+    usage
+    ;;
+  esac
+done

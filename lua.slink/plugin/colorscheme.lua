0e693c542d541cab577515e8b90b58bb82e74af6
fcolic89 <filip.colic00@gmail.com>
2025-03-30 00:37:34 +0100

    (v0.2.0): Colorsheme update

--- lua.slink/plugin/colorscheme.lua
+++ lua.slink/plugin/colorscheme.lua
@@ -3,8 +3,14 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme('kanagawa-dragon')
+      vim.cmd.colorscheme('moonfly')
     end
   },
 }
